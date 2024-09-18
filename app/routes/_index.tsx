6f5dd1b7ac0f7f4c4290f54d5a75f03835cae14e
f <48589369+fabiogaliano@users.noreply.github.com>
2024-09-18 19:31:54 +0100

    feat: add spotify auth

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
+import { Form, useLoaderData } from '@remix-run/react';
+import { spotifyStrategy } from '~/services/auth.server';
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,9 +9,29 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export default function Index() {
-  return (<div>hi</div>)
-
+export async function loader({ request }: LoaderFunctionArgs) {
+  return spotifyStrategy.getSession(request);
 }
 
+export default function Index() {
+  const data = useLoaderData<typeof loader>();
+  const user = data?.user;
 
+  return (
+    <div style={{ textAlign: 'center', padding: 20 }}>
+      <h2>Welcome to Remix!</h2>
+      <p>
+        <a href="https://docs.remix.run">Check out the docs</a> to get
+        started.
+      </p>
+      {user ? (
+        <p>You are logged in as: {user.name}</p>
+      ) : (
+        <p>You are not logged in yet!</p>
+      )}
+      <Form action={user ? '/logout' : '/auth/spotify'} method="post">
+        <button>{user ? 'Logout' : 'Log in with Spotify'}</button>
+      </Form>
+    </div>
+  );
+}
